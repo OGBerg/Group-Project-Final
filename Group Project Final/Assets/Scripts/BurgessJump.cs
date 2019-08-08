5f14054df6ee52a43a434227ce6ed68740b35031
Alexander Gould <ag4536@nyu.edu>
2019-08-08 15:13:19 -0400

    physics testing

---
+++ Group Project Final/Assets/Scripts/BurgessJump.cs
@@ -0,0 +1,122 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BurgessJump : MonoBehaviour {
+
+	public float speed;
+	public float jumpSpeed;
+	Vector2 movement;
+
+	public float fallingGravityModifier;
+	public int jumpHoldFrames;
+	int currentJumpHoldFrames;
+
+	Rigidbody2D rb;
+	//Animator anim;
+	//SpriteRenderer sr;
+
+	bool onGround = true;
+	bool isJumping = false;
+	
+
+	KeyCode jumpKey;
+
+	void Start () {
+		// assigning components to our global variables
+		rb = GetComponent<Rigidbody2D>();
+		//anim = GetComponent<Animator>();
+		//sr = GetComponent<SpriteRenderer>();
+
+		jumpKey = KeyCode.Space;
+		currentJumpHoldFrames = jumpHoldFrames;
+	}
+	
+	void Update () {
+		MovePlayer();
+		//HandleSpriteDirection();
+		//SetAnimationParameters();
+	}
+
+	void FixedUpdate () {
+		// using the movement vector that gets assigned in update here in fixedupdate to apply force to the rigidbody
+		if (isJumping) {
+			rb.AddForce(Vector2.up * jumpSpeed * Time.fixedDeltaTime, ForceMode2D.Impulse);
+			isJumping = false;
+		}
+		else if (!onGround && Input.GetKey(jumpKey) && currentJumpHoldFrames > 0) {
+			rb.AddForce(Vector2.up * jumpSpeed * 0.5f * Time.fixedDeltaTime, ForceMode2D.Impulse);
+		}
+
+
+		if (!onGround && rb.velocity.y < 0) {
+			// this means we're in the air and falling. i want to apply some additional gravity in this case;
+			Vector2 v = rb.velocity;
+			v.y -= fallingGravityModifier * Time.fixedDeltaTime;
+			rb.velocity = v;			
+		}
+
+
+		if (onGround) {
+			rb.AddForce(movement * speed * Time.fixedDeltaTime);	
+		}
+		else {
+			rb.AddForce(movement * speed * 0.5f * Time.fixedDeltaTime);
+		}
+	}
+
+	void MovePlayer () {
+		// assigning our desired movement vector based on player input in the update loop
+		movement = Vector2.zero;
+
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			movement += Vector2.left;
+		}
+
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			movement += Vector2.right;
+		}
+
+		if (Input.GetKeyDown(jumpKey) && onGround) {
+			isJumping = true;
+		}
+
+		if (Input.GetKeyUp(jumpKey) && !onGround) {
+			currentJumpHoldFrames = 0;
+		}
+
+		if (Input.GetKey(jumpKey) && !onGround) {
+			currentJumpHoldFrames--;
+		}
+
+	}
+
+
+
+	void OnCollisionEnter2D (Collision2D c) {
+		if (c.gameObject.tag == "ground") {
+			onGround = true;
+			currentJumpHoldFrames = jumpHoldFrames;
+		}
+	}
+
+	void OnCollisionExit2D (Collision2D c) {
+		if (c.gameObject.tag == "ground") {
+			onGround = false;
+		}	
+	}
+
+
+
+	// void HandleSpriteDirection () {
+	// 	if (Mathf.Abs(rb.velocity.x) > 1) {
+	// 		sr.flipX = (rb.velocity.x < 0);
+	// 	}
+	// }
+
+	// void SetAnimationParameters () {
+	// 	bool isRunning = (movement.x != 0);
+	// 	anim.SetBool("running", isRunning);
+	// 	anim.SetBool("jumping", !onGround);
+	// }
+}
---
+++ Group Project Final/Assets/Scripts/TestJump.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestJump : MonoBehaviour
+{
+	public float speed;
+	public float airSpeed;
+	public float launchSpeed;
+
+	public AnimationCurve slowdownCurve;
+	public float slowdownMagnitude; //keep at 1?
+	public AnimationCurve speedupCurve;
+	public float speedupMagnitude; //keep at 1?
+	//x += (target - x) * 0.3f;
+
+	Vector2 movement;
+	Vector2 launchDirection;
+
+	Rigidbody2D rb;
+	bool onGround = true;
+	bool isLaunching = false;
+	bool slowdown = false;
+	bool speedup = false;
+	bool impact = false;
+
+	float slowdownStartTime;
+	public float slowdownMaxDuration;
+	float speedupStartTime;
+	public float speedupMaxDuration;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        movement = Vector2.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(onGround)
+        {
+        	if(Input.GetKey(KeyCode.LeftArrow)) launchDirection = Vector2.left;
+        	if(Input.GetKey(KeyCode.RightArrow)) launchDirection = Vector2.right;
+        	movement += launchDirection;
+        	if(Input.GetKey(KeyCode.Space))
+        	{
+        		isLaunching = true;
+        		// onGround = false;
+        	}
+        }
+        else
+        {
+        	if(Input.GetKeyDown(KeyCode.Space))
+        	{
+        		speedup = false;
+        		slowdown = true;
+        		slowdownStartTime = Time.time;
+        	}
+        	if(Input.GetKeyUp(KeyCode.Space))
+        	{
+        		slowdown = false;
+        		speedup = true;
+        		speedupStartTime = Time.time;
+        	}
+        }
+    }
+
+    void FixedUpdate()
+    {
+    	if(isLaunching)
+    	{
+    		rb.AddForce(Vector2.up * launchSpeed * Time.fixedDeltaTime, ForceMode2D.Impulse);
+    		isLaunching = false;
+    	}
+
+    	if(onGround)
+    	{
+    		slowdown = false;
+    		speedup = false;
+    		if(impact)
+    		{
+    			rb.velocity = Vector2.zero; //add curve?
+    			//play anim here
+    			impact = false;
+    		}
+    		rb.AddForce(movement * speed * Time.fixedDeltaTime);
+    	}
+    	else
+    	{
+    		rb.velocity = new Vector2(launchDirection.x * airSpeed, rb.velocity.y);
+    		if(rb.velocity.y < 0)
+    		{
+    			Vector2 velocity = rb.velocity;
+    			velocity.y = 0;
+    			rb.velocity = velocity;
+    		}
+    		impact = true;
+    		if(slowdown)
+    		{
+    			Vector2 velocity = rb.velocity;
+    			velocity.x -= slowdownCurve.Evaluate(Mathf.Clamp01((Time.time - slowdownStartTime) / slowdownMaxDuration)) * slowdownMagnitude;
+    			rb.velocity = velocity;
+    		}
+    		if(speedup)
+    		{
+    			Vector2 velocity = rb.velocity;
+    			velocity.x += speedupCurve.Evaluate(Mathf.Clamp01((Time.time - speedupStartTime) / speedupMaxDuration)) * speedupMagnitude;
+    			rb.velocity = velocity;
+    		}
+    	}
+    }
+
+    void OnCollisionEnter2D(Collision2D c)
+    {
+    	if(c.gameObject.tag == "ground") onGround = true;
+    }
+
+    void OnCollisionExit2D(Collision2D c)
+    {
+    	if(c.gameObject.tag == "ground") onGround = false;
+    }
+}
