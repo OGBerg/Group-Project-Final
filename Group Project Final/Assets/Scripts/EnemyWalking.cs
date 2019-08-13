ac598f2319f47a0be69568cc6e28e5f8f3157273
OGBerg <bergholdino@me.com>
2019-08-13 15:43:39 -0400

    Added Enemy and Pickups

---
+++ Group Project Final/Assets/Scripts/EnemyWalking.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyWalking : MonoBehaviour
+{
+    SpriteRenderer mySprite;
+
+    public float resetTime = 3f; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mySprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        resetTime -= Time.deltaTime;
+        if (resetTime <= 0)
+        {
+            mySprite.flipX = false; 
+            resetTime = 3f;
+            
+
+        }
+        //else
+        //{
+        //    mySprite.flipX = false;
+        //}
+    }
+}
