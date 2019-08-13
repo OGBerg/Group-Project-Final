06f4459b4be19b18ac3b8e016cf59ad44a9fe62a
Alexander Gould <ag4536@nyu.edu>
2019-08-13 15:45:05 -0400

    asset

---
+++ Group Project Final/Assets/Scripts/MoveBackground.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBackground : MonoBehaviour
+{
+	public float offset;
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.UpArrow)) transform.position = new Vector2(transform.position.x, transform.position.y - offset);
+        if(Input.GetKey(KeyCode.DownArrow)) transform.position = new Vector2(transform.position.x, transform.position.y + offset);
+        if(Input.GetKey(KeyCode.LeftArrow)) transform.position = new Vector2(transform.position.x + offset, transform.position.y);
+        if(Input.GetKey(KeyCode.RightArrow)) transform.position = new Vector2(transform.position.x - offset, transform.position.y);
+    }
+}
