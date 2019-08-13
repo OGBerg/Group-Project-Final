af126fb5ca27431d02a70064e68a52e70be577c0
OGBerg <bergholdino@me.com>
2019-08-13 16:28:53 -0400

    camera flash script

---
+++ Group Project Final/Assets/Scripts/camerflashscript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camerflashscript : MonoBehaviour
+{
+
+    SpriteRenderer spriterenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriterenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        spriterenderer.enabled |= collision.gameObject.tag == "Player";
+    }
+}
