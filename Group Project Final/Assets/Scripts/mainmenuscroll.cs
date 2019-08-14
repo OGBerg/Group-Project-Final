487c079b4fc14785351e5c81e62d7f95cc89b497
OGBerg <bergholdino@me.com>
2019-08-14 17:51:08 -0400

    Animated the Main Menu a lil bit

---
+++ Group Project Final/Assets/Scripts/mainmenuscroll.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mainmenuscroll : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var pos = transform.position;
+        pos.x += .03f;
+        transform.position = pos;
+    }
+
+    private void OnTriggerEnter2D (Collider2D collision)
+    {
+        if (collision.gameObject.tag == "SpecialWall")
+        {
+            Debug.Log("hi");
+            var pos = transform.position;
+            pos.x = -5.7f;
+            pos.y = 1.053613f;
+            transform.position = pos;
+
+
+
+        }
+    }
+}
