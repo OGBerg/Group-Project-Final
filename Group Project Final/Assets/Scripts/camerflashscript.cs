0ec1ba4ad1b269a699abf0b5511f57f7a415f83e
OGBerg <bergholdino@me.com>
2019-08-14 16:38:16 -0400

    Reload current level + removed level 2

--- Group Project Final/Assets/Scripts/camerflashscript.cs
+++ Group Project Final/Assets/Scripts/camerflashscript.cs
@@ -1,16 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class camerflashscript : MonoBehaviour
 {
 
     SpriteRenderer spriterenderer;
+    public float reloadTime;
 
     // Start is called before the first frame update
     void Start()
     {
         spriterenderer = GetComponent<SpriteRenderer>();
+        reloadTime = 1f;
     }
 
     // Update is called once per frame
@@ -21,6 +24,25 @@ public class camerflashscript : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        spriterenderer.enabled |= collision.gameObject.tag == "Player";
+        if (collision.gameObject.tag == "Player")
+        {
+            spriterenderer.enabled = true;
+            ReloadCurrentScene();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        spriterenderer.enabled &= collision.gameObject.tag != "Player";
+    }
+
+    public void ReloadCurrentScene()
+    {
+       
+        string sceneName = SceneManager.GetActiveScene().name;
+
+        
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
     }
 }
+
