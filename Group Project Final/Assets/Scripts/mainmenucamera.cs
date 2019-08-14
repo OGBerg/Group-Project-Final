6061061bf2b37c83a55be1f120e28d14a65c937f
OGBerg <bergholdino@me.com>
2019-08-14 17:30:34 -0400

    Main Menu changes, camera flashing for menu

---
+++ Group Project Final/Assets/Scripts/mainmenucamera.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mainmenucamera : MonoBehaviour
+{
+
+
+    SpriteRenderer spriterenderer;
+
+    // this is the UI.Text or other UI element you want to toggle
+
+
+    public float interval = 1f;
+    public float startDelay = 0.5f;
+    public bool currentState = true;
+    public bool defaultState = true;
+    bool isBlinking = false;
+
+
+    void Start()
+    {
+        spriterenderer = GetComponent<SpriteRenderer>();
+        spriterenderer.enabled = defaultState;
+        StartBlink();
+    }
+
+    public void StartBlink()
+    {
+        // do not invoke the blink twice - needed if you need to start the blink from an external object
+        if (isBlinking)
+            return;
+
+        if (spriterenderer != null)
+        {
+            isBlinking = true;
+            InvokeRepeating("ToggleState", startDelay, interval);
+        }
+    }
+
+    public void ToggleState()
+    {
+        spriterenderer.enabled = !spriterenderer.enabled;
+
+       
+    }
+
+}
+
+
