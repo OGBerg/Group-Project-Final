3689f4ba5e5d709b084e9761d146d7c88db215f8
OGBerg <bergholdino@me.com>
2019-08-14 23:36:24 -0400

    Added level scenes

--- Group Project Final/Assets/Scripts/pickup.cs
+++ Group Project Final/Assets/Scripts/pickup.cs
@@ -1,14 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class pickup : MonoBehaviour
 {
+    
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
             Destroy(gameObject);
+            
         }
+    }
+
+    void Update()
+    {
+        
     }
 }
